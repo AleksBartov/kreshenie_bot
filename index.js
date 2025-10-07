b5313276ae7670f1800a6d1cf8d5599058acede1
AleksBartov <aleksandrbartov@yandex.ru>
2025-10-08 00:47:24 +0300

    next commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import { config } from 'dotenv'
 
-import { Telegraf} from 'telegraf'
+
+import {Telegraf} from 'telegraf'
+import { message} from 'telegraf/filters'
 
 config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
-bot.start(ctx=>{
-    ctx.reply('let us start with env!')
-})
-
-
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('хочу быть крестным', (ctx) => ctx.reply('Добро! но для начала подготовьтесь'))
 bot.launch()
 
-
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
