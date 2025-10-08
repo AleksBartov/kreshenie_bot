0b5b0485c2c8c09e1929b784294c3c8d9975f0db
AleksBartov <aleksandrbartov@yandex.ru>
2025-10-09 00:37:33 +0300

    from test

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears('хочу быть крестным', (ctx) => ctx.reply('Добро! но для начала подготовьтесь'))
+bot.hears('хочу быть крестным', (ctx) => ctx.reply('Добро!Дружище но для начала подготовьтесь'))
 bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
