d57b01027fc567f387c3ee444b6e5816ac491cda
AleksBartov <aleksandrbartov@yandex.ru>
2025-10-10 00:04:34 +0300

    first example

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 import { config } from 'dotenv'
 
 
-import {Telegraf} from 'telegraf'
-import { message} from 'telegraf/filters'
+import {Telegraf, Markup} from 'telegraf'
 
 config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears('хочу быть крестным', (ctx) => ctx.reply('Добро!Дружище но для начала подготовьтесь'))
-bot.launch()
+bot.start((ctx) => {
+    return ctx.reply('Ну что, начнем?',
+        Markup.keyboard([
+            [Markup.button.pollRequest('🙋‍♀️ Создать опросник', 'regular'),
+            Markup.button.pollRequest('🙋‍♀️ Создать викторину', 'quiz')],
+            ['hi', '/poll', '/quiz', '/inline']
+        ]).oneTime().resize())
+})
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+bot.help(ctx => ctx.reply(`Попробуйте команды:
+/poll
+/quiz
+/inline
+или напишите hi
+или отправьте мне стикер`))
+
+bot.on('sticker', ctx => ctx.reply('🥰'))
+
+bot.hears('hi', ctx => ctx.reply(`Hi??? И это все, ${ctx.chat.first_name}, что ты можешь сказать?`))
+
+bot.command('poll', ctx => ctx.replyWithPoll('Твой любимый язык программирования?', [
+    'JavaScript', 'Python', 'C++', 'Lua', 'Pascal', 'Что такое "язык программирования'], { is_anonymous: false }))
+
+bot.command('quiz', ctx=>ctx.replyWithQuiz('1 + 1 = ?', ['4', '3.14', '2', '💩'], {correct_option_id: 2}))
+
+bot.command('inline', ctx=>ctx.reply('Какой-то текст', Markup.inlineKeyboard([
+    Markup.button.callback('Скажу "ДА"', 'ok'),
+    Markup.button.callback('Скажу "Нет"', 'cancel'),
+])))
+
+bot.action('ok', (ctx, next)=> ctx.answerCbQuery('Ответ Да').then(()=>next()))
+bot.action('cancel', (ctx, next)=> ctx.answerCbQuery('Ответ Нет', {show_alert: true}).then(()=>next()))
+
+bot.launch()
 
